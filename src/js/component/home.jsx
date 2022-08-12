b1d2f909a2da64bda6a99fb92ab042e716d59387
Kaiki <alejandrobisuteria@gmail.com>
2022-08-12 18:29:41 +0000

    Traffic-Light.v1

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,39 @@
 import React from "react";
-
+import { useState, useEffect  } from "react";
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [color, setColor] = useState("");
+    const [mostrar, serMostrar] = useState(false);
+    const [cambio, setCambio] = useState(0);
+    let array = ["nada","red","orange","green","purple"]
+	
+    useEffect(() => {
+      
+        if(cambio>4){
+         setCambio(1)
+        }else{
+         setColor(array[cambio])
+        }
+      }, [cambio]);
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+        <>
+        <div className="mt-5 d-flex justify-content-center">
+		<ul className="semaforo rojo naranja verde purple">
+        <li onClick={() =>setColor("red")} id={color == "red"?"red":""} ></li>
+        <li onClick={() =>setColor("orange")} id={color == "orange"?"orange":""} ></li>
+        <li onClick={() =>setColor("green")} id={color == "green"?"green":""} ></li>
+        {mostrar?<li onClick={() =>setColor("purple")} id={color == "purple"?"purple":""} ></li>:""}
+    </ul>
+    </div>
+    <div className="w-25 mx-auto mt-5 d-flex justify-content-around">
+        <button className="btn btn-success" onClick={() =>serMostrar(true)}>Mostrar</button>
+        <button className="btn btn-danger" onClick={() =>serMostrar(false)}>Ocultar</button>
+        <button className="btn btn-primary" onClick={() =>setCambio(cambio+1)}>Cambiar</button>
+    </div>
+     </>
 	);
 };
 
